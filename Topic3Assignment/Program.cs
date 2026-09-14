9847175d1e1f51523d492ed1f39dabd943118344
362582108 <362582108@2260I-L29-016>
2026-09-14 12:37:14 -0400

    Add project files.

---
+++ Topic3Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Topic3Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, lastName, eyeColor, hairColor;
+            int age, height, weight;
+
+            name = "Chungy";
+            lastName = " McFife";
+            age = 427;     // autofill says This is a very old man
+            height = 324;  // autofill says autofill says This is a very tall man
+            weight = 94;   // rule of threes
+            eyeColor = "Borange";
+            hairColor = "Baldle";
+
+            Console.WriteLine("Let's talk about " + name + lastName + ".");
+            Console.WriteLine("It's " + height + " inches tall.");
+            Console.WriteLine("It's " + weight + " pounds heavy.");
+            Console.WriteLine("That is a weight of all time.");
+            Console.WriteLine("It's eyes are " + eyeColor + " and hair is " + hairColor);
+            
+            // This line is Tricky. Say HI!
+            Console.WriteLine("If I add " + age + ", " + height + ", and " + weight + " I get " + (age + height + weight) + ".");
+        }
+    }
+}
