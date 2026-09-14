59ff7365f3d7cfd1054dcd82574309320dd02a06
362582108 <362582108@2260I-L29-016>
2026-09-14 13:16:49 -0400

    Removed Captured Soul

--- Topic3Assignment/Program.cs
+++ Topic3Assignment/Program.cs
@@ -10,21 +10,24 @@ namespace Topic3Assignment
             int age, height, weight;
 
             name = "Chungy";
-            lastName = " McFife";
+            lastName = "McFife";
             age = 427;     // autofill says This is a very old man
             height = 324;  // autofill says autofill says This is a very tall man
             weight = 94;   // rule of threes
             eyeColor = "Borange";
             hairColor = "Baldle";
-
-            Console.WriteLine("Let's talk about " + name + lastName + ".");
-            Console.WriteLine("It's " + height + " inches tall.");
-            Console.WriteLine("It's " + weight + " pounds heavy.");
+            
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Let's talk about " + name + " " + lastName + ".");
+            Console.WriteLine("It's " + height + " inches tall, or ");
+            Console.WriteLine("It's " + weight + " pounds heavy, or ");
             Console.WriteLine("That is a weight of all time.");
             Console.WriteLine("It's eyes are " + eyeColor + " and hair is " + hairColor);
             
             // This line is Tricky. Say HI!
             Console.WriteLine("If I add " + age + ", " + height + ", and " + weight + " I get " + (age + height + weight) + ".");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Black;
         }
     }
 }
